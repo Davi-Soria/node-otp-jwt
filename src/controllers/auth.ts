56e1ae1d5b72fe685dade63a0d8dd4190498ab9c
Davi Soria <davisoriamaia@gmail.com>
2025-07-07 09:28:05 -0300

    generating otp

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
 import { authSignInSchema } from "../schemas/auth.signin";
 import { getUserByEmail } from "../services/user";
+import { generateOtp } from "../services/otp";
 
 export const signin: RequestHandler = async (req, res) => {
     // Validar os dados recebidos;
@@ -16,7 +17,9 @@ export const signin: RequestHandler = async (req, res) => {
         return
     }
     // Gerar um código OTP para este usuário
+    const otp = await generateOtp(user.id);
+
     // Enviar um email para o usuário com o código
     // Devolve o ID do código OTP
-
+    res.json({ id: otp.id });
 }
